8e96620ea4ea9d30bb30bf2bce5d1b71ca863004
Zak Pruitt <pruittzn@gmail.com>
2024-06-15 15:49:11 -0500

    lesson 2

--- src/lesson_2.rs
+++ src/lesson_2.rs
@@ -1,15 +1,33 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn guessing_game() {
-    println!("Guess the number!");
-    print!("Please input your guess: ");
+    loop {
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        println!("Guess the number!");
+        println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; // exits the loop
+            }
+        }
+    }
 }
 
 pub fn execute_lesson() {
